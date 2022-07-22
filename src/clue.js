ed2f9ad2d8fa6e037a84aae2db9071023c0d0488
Priscille Louis-rose <priscille.louisrose@gmail.com>
2022-07-22 19:47:41 +0200

    lab day 2 extra done

--- src/clue.js
+++ src/clue.js
@@ -2,25 +2,173 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    {
+        firstName: 'Jacob',
+        lastName: 'Green',
+        occupation: 'priest',
+        age: 63,
+        color: 'green',
+        
+    },
+    {
+        firstName: 'Scarlett',
+        lastName: 'Scarlett',
+        occupation: 'femme fatale',
+        age: 31,
+        color: 'red',
+        
+    },
+    {
+        firstName: 'Margaret',
+        lastName: 'White',
+        occupation: 'maid',
+        age: 46,
+        color: 'white',
+        
+    },
+    {
+        firstName: 'Edgar',
+        lastName: 'Plum',
+        occupation: 'professor',
+        age: 35,
+        color: 'purple',
+        
+    },
+    {
+        firstName: 'Algernon',
+        lastName: 'Mustard',
+        occupation: 'military',
+        age: 50,
+        color: 'yellow',
+        
+    },
+    {
+        firstName: 'Henrietta',
+        lastName: 'Peacock',
+        occupation: 'ornithologist ',
+        age: 62,
+        color: 'green',
+        
+    }
+
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    {
+        name: "Bathroom" 
+    }, 
+    {
+        name: "Study" 
+    },
+    {
+        name: "Dining Room" 
+    },
+    {
+        name: "Game Room" 
+    },
+    {
+        name: "Bedroom1" 
+    },
+    {
+        name: "Bedroom2" 
+    },
+    {
+        name: "Bedroom3" 
+    },
+    {
+        name: "Bedroom4" 
+    },
+    {
+        name: "Living Room" 
+    },
+    {
+        name: "Kitchen" 
+    },
+    {
+        name: "Courtyard" 
+    },
+    {
+        name: "Cellar" 
+    },
+    {
+        name: "Attic" 
+    },
+    {
+        name: "Secret Room" 
+    },
+    {
+        name: "Toilets" 
+    },
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+    {
+        name: 'Revolver',
+        weight: 600
+    },
+    {
+        name: 'Dagger',
+        weight: 400
+    },
+    {
+        name: 'Lead Pipe',
+        weight: 800
+    },
+    {
+        name: 'Rope',
+        weight: 200
+    },
+    {
+        name: 'Candlestick',
+        weight: 1500
+    },
+    {
+        name: 'Wrench',
+        weight: 600
+    },
+    {
+        name: 'Rifle',
+        weight: 1200
+    },
+    {
+        name: 'Kitchen knife',
+        weight: 400
+    },
+    {
+        name: 'Cricket bat',
+        weight: 1100
+    }
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+    return randomElement = array[Math.floor(Math.random() * array.length)]
+}
 
-function pickMystery() {}
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspectsArray),
+        weapon: selectRandom(weaponsArray),
+        room: selectRandom(roomsArray)
+    }
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(envelope) {
+    return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name} !!!`
+}
 
+
+///console.log(selectRandom(suspectsArray))
+const mystery = pickMystery()
+console.log(mystery)
+console.log(revealMystery(mystery))
